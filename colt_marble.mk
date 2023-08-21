dfdb4fcc83cd7752fdc0b81df393f9278569cd62
vherawidatama <vherawidatama@gmail.com>
2023-08-21 10:36:22 +0000

    first commit

--- colt_marble.mk
+++ colt_marble.mk
@@ -9,15 +9,32 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/colt/config/common.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
 ## Device identifier
 PRODUCT_DEVICE := marble
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := colt_marble
 PRODUCT_MANUFACTURER := Xiaomi
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# ADD-ON
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := false
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+
+# Core_Gapps Include
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+
+# Maintainer
+PRODUCT_SYSTEM_PROPERTIES += \
+     ro.colt.maintainer=
+
+# ColtOS Maintainer String
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.colt.maintainer=
